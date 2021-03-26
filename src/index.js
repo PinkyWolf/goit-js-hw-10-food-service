e94e0e46bb07b2b79b351f17530c0f4d38fc23c6
Serhii Hult <gyltsergiy@gmail.com>
2021-03-26 14:22:51 +0200

    Підключені імпорти в джс та розмітка на сторінку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import listTemplate from "../src/handlebars/card.hbs"
+import menuCard from "../src/menu.json"
 import './styles.css';
+
+const menuRef = document.querySelector('.js-menu')
+
+const menuMarkup = createMenu(menuCard)
+
+menuRef.innerHTML = menuMarkup
+
+function createMenu(menuCard) {
+return menuCard.map(listTemplate).join('')
+}
